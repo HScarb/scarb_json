a844c4533b9094e463d4dc7fdd034e852f95c6bb
Scarb <jjhfen00@163.com>
2016-11-02 15:52:51 +0800

    finish chapter02 answer

--- json-learn/scarbjson.c
+++ json-learn/scarbjson.c
@@ -1,9 +1,13 @@
 #include "scarbjson.h"
 #include <assert.h>		// assert()
-#include <stdlib.h>		// NULL
+#include <errno.h>		// errno, ERANGE
+#include <math.h>		// HUGE_VAL
+#include <stdlib.h>		// NULL,strtod
 
-// c != ch, report error
+// *c->json != ch, report error, else c->json++
 #define EXPECT(c, ch)		do{ assert(*c->json == (ch)); c->json++; } while(0)
+#define ISDIGIT(ch)			((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)		((ch) >= '1' && (ch) <= '9')
 
 /* 为了减少解析函数之间传递多个参数,把这些数据都放进一个结构体 */
 typedef struct
@@ -12,7 +16,7 @@ typedef struct
 }scarb_context;
 
 /*
-	解析空格，
+	解析空白，
 */
 static void scarb_parse_whitespace(scarb_context* c)
 {
@@ -37,7 +41,7 @@ static int scarb_parse_false(scarb_context* c, scarb_value* v) {
 	if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
 		return SCARB_PARSE_INVALID_VALUE;
 	c->json += 4;
-	v->type = SCARB_TRUE;
+	v->type = SCARB_FALSE;
 	return SCARB_PARSE_OK;
 }
 
@@ -50,15 +54,74 @@ static int scarb_parse_null(scarb_context* c, scarb_value* v) {
 	return SCARB_PARSE_OK;
 }
 
+/** 解析文字，包括 true/false/null
+*	c为要解析的文字
+*	v为解析的结果值
+*	literal为要解析的期望文字
+*	type为解析成功赋给值的类型
+*/
+static int scarb_parse_literal(scarb_context* c, scarb_value* v, const char* literal, scarb_type type)
+{
+	size_t i;
+	EXPECT(c, literal[0]);
+	for (i = 1; literal[i + 1]; i++)
+	{
+		if (c->json[i] != literal[i + 1])
+			return SCARB_PARSE_INVALID_VALUE;
+	}
+	c->json += i;
+	v->type = type;
+	return SCARB_PARSE_OK;
+}
+
+/* 解析数 */
+static int scarb_parse_number(scarb_context* c, scarb_value* v)
+{
+	const char* p = c->json;		// 表示当前的解析字符位置
+/* 校验数字,排除json不允许的格式 */
+	/* 负号 */
+	if (*p == '-') p++;
+	/* 整数 */
+	if (*p == '0') p++;				// 跳过单个0
+	else
+	{
+		if (!ISDIGIT1TO9(*p)) return SCARB_PARSE_INVALID_VALUE;		// 第一个数必定是1到9，否则不合法
+		for (p++; ISDIGIT(*p); p++);// 跳过剩余digit
+	}
+	/* 小数 */
+	if(*p == '.')
+	{
+		p++;
+		if (!ISDIGIT(*p)) return SCARB_PARSE_INVALID_VALUE;			// 检测是否至少有一digit，没有就返回错误
+		for (p++; ISDIGIT(*p); p++);								// 跳过剩余digit
+	}
+	/* 指数 */
+	if(*p == 'e' || *p == 'E')
+	{
+		p++;														// 跳过e
+		if (*p == '+' || *p == '-') p++;							// 如果有一个正或负号，跳过
+		if (!ISDIGIT(*p))return SCARB_PARSE_INVALID_VALUE;			// 然后和小数的逻辑一样
+		for (p++; ISDIGIT(*p); p++);
+	}
+	errno = 0;
+	v->n = strtod(c->json, NULL);
+	/* 如果数字过大，返回NUMBER_TOO_BIG */
+	if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+		return SCARB_PARSE_NUMBER_TOO_BIG;
+	v->type = SCARB_NUMBER;
+	c->json = p;
+	return SCARB_PARSE_OK;
+}
+
 static int scarb_parse_value(scarb_context* c, scarb_value*v)
 {
 	switch(*c->json)
 	{
-	case 't':return scarb_parse_true(c, v);
-	case 'f':return scarb_parse_false(c, v);
-	case 'n':return scarb_parse_null(c, v);
+	case 't':return scarb_parse_literal(c, v, "true", SCARB_TRUE);
+	case 'f':return scarb_parse_literal(c, v, "false", SCARB_FALSE);
+	case 'n':return scarb_parse_literal(c, v, "null", SCARB_NULL);
+	default:return scarb_parse_number(c, v);
 	case '\0':return SCARB_PARSE_EXPECT_VALUE;
-	default:return SCARB_PARSE_INVALID_VALUE;
 	}
 }
 
@@ -74,13 +137,23 @@ int scarb_parse(scarb_value* v, const char* json)
 	{
 		scarb_parse_whitespace(&c);
 		if (*c.json != '\0')
+		{
+			v->type = SCARB_NULL;
 			ret = SCARB_PARSE_ROOT_NOT_SINGULAR;
+		}
 	}
 	return ret;
 }
 
+/** getter **/
 scarb_type scarb_get_type(const scarb_value* v)
 {
 	assert(v != NULL);
 	return v->type;
+}
+
+double scarb_get_number(const scarb_value* v)
+{
+	assert(v != NULL && v->type == SCARB_NUMBER);		// 仅当type==NULBER时，n才表示json数字的数值
+	return v->n;
 }
--- json-learn/test.c
+++ json-learn/test.c
@@ -13,13 +13,14 @@ static int test_pass = 0;
 		if(equality)\
 			test_pass++;\
 		else{\
-			fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
+			fprintf(stderr, "%s: line %d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
 			main_ret = 1;\
 		}\
 	} while(0)
 
 // 每次使用这个宏时，如果 expect != actual（预期值不等于实际值），便会输出错误信息。
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
+#define EXPECT_EQ_DOUBLE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
 
 static void test_parse_null()
 {
@@ -44,34 +45,94 @@ static void test_parse_false() {
 	EXPECT_EQ_INT(SCARB_FALSE, scarb_get_type(&v));
 }
 
+/* 测试数值的宏，expect为测试期望值，json为测试的json值 */
+#define TEST_NUMBER(expect, json)\
+	do{\
+		scarb_value v;\
+		EXPECT_EQ_INT(SCARB_PARSE_OK, scarb_parse(&v, json));/*解析成scarb_value*/\
+		EXPECT_EQ_INT(SCARB_NUMBER, scarb_get_type(&v));/*判断类型是否为number*/\
+		EXPECT_EQ_DOUBLE(expect, scarb_get_number(&v));/*测试number值*/\
+	} while(0)
+
+static void test_parse_number() {
+	TEST_NUMBER(0.0, "0");
+	TEST_NUMBER(0.0, "-0");
+	TEST_NUMBER(0.0, "-0.0");
+	TEST_NUMBER(1.0, "1");
+	TEST_NUMBER(-1.0, "-1");
+	TEST_NUMBER(1.5, "1.5");
+	TEST_NUMBER(-1.5, "-1.5");
+	TEST_NUMBER(3.1416, "3.1416");
+	TEST_NUMBER(1E10, "1E10");
+	TEST_NUMBER(1e10, "1e10");
+	TEST_NUMBER(1E+10, "1E+10");
+	TEST_NUMBER(1E-10, "1E-10");
+	TEST_NUMBER(-1E10, "-1E10");
+	TEST_NUMBER(-1e10, "-1e10");
+	TEST_NUMBER(-1E+10, "-1E+10");
+	TEST_NUMBER(-1E-10, "-1E-10");
+	TEST_NUMBER(1.234E+10, "1.234E+10");
+	TEST_NUMBER(1.234E-10, "1.234E-10");
+	TEST_NUMBER(0.0, "1e-10000"); /* must underflow */
+
+	/* 边界值测试 */
+	TEST_NUMBER(1.0000000000000002, "1.0000000000000002"); /* the smallest number > 1 */
+	TEST_NUMBER(4.9406564584124654e-324, "4.9406564584124654e-324"); /* minimum denormal */
+	TEST_NUMBER(-4.9406564584124654e-324, "-4.9406564584124654e-324");
+	TEST_NUMBER(2.2250738585072009e-308, "2.2250738585072009e-308");  /* Max subnormal double */
+	TEST_NUMBER(-2.2250738585072009e-308, "-2.2250738585072009e-308");
+	TEST_NUMBER(2.2250738585072014e-308, "2.2250738585072014e-308");  /* Min normal positive double */
+	TEST_NUMBER(-2.2250738585072014e-308, "-2.2250738585072014e-308");
+	TEST_NUMBER(1.7976931348623157e+308, "1.7976931348623157e+308");  /* Max double */
+	TEST_NUMBER(-1.7976931348623157e+308, "-1.7976931348623157e+308");
+}
+
+/* 测试相应错误类型的宏，error为要测试的错误，json为测试的json值 */
+#define TEST_ERROR(error, json)\
+	do{\
+		scarb_value v;\
+		v.type = SCARB_FALSE;\
+		EXPECT_EQ_INT(error, scarb_parse(&v, json));\
+		EXPECT_EQ_INT(SCARB_NULL, scarb_get_type(&v));\
+	} while(0)
+
+
+/* 测试解析只含空白的值，返回PARSE_EXPECT_VALUE */
 static void test_parse_expect_value() {
-	scarb_value v;
-
-	v.type = SCARB_TRUE;
-	EXPECT_EQ_INT(SCARB_PARSE_EXPECT_VALUE, scarb_parse(&v, ""));
-	EXPECT_EQ_INT(SCARB_NULL, scarb_get_type(&v));
-
-	v.type = SCARB_TRUE;
-	EXPECT_EQ_INT(SCARB_PARSE_EXPECT_VALUE, scarb_parse(&v, " "));
-	EXPECT_EQ_INT(SCARB_NULL, scarb_get_type(&v));
+	TEST_ERROR(SCARB_PARSE_EXPECT_VALUE, "");
+	TEST_ERROR(SCARB_PARSE_EXPECT_VALUE, " ");
 }
 
+/* 测试解析无效的值，返回PARSE_INVALID_VALUE */
 static void test_parse_invalid_value() {
-	scarb_value v;
-	v.type = SCARB_TRUE;
-	EXPECT_EQ_INT(SCARB_PARSE_INVALID_VALUE, scarb_parse(&v, "nul"));
-	EXPECT_EQ_INT(SCARB_NULL, scarb_get_type(&v));
+	TEST_ERROR(SCARB_PARSE_INVALID_VALUE, "nul");
+	TEST_ERROR(SCARB_PARSE_INVALID_VALUE, "?");
 
-	v.type = SCARB_TRUE;
-	EXPECT_EQ_INT(SCARB_PARSE_INVALID_VALUE, scarb_parse(&v, "?"));
-	EXPECT_EQ_INT(SCARB_NULL, scarb_get_type(&v));
+	/* invalid number */
+	TEST_ERROR(SCARB_PARSE_INVALID_VALUE, "+0");
+	TEST_ERROR(SCARB_PARSE_INVALID_VALUE, "+1");
+	TEST_ERROR(SCARB_PARSE_INVALID_VALUE, ".123"); /* at least one digit before '.' */
+	TEST_ERROR(SCARB_PARSE_INVALID_VALUE, "1.");   /* at least one digit after '.' */
+	TEST_ERROR(SCARB_PARSE_INVALID_VALUE, "INF");
+	TEST_ERROR(SCARB_PARSE_INVALID_VALUE, "inf");
+	TEST_ERROR(SCARB_PARSE_INVALID_VALUE, "NAN");
+	TEST_ERROR(SCARB_PARSE_INVALID_VALUE, "nan");
 }
 
+/* 测试解析，返回ROOT_NOT_SINGULAR错误 */
 static void test_parse_root_not_singular() {
-	scarb_value v;
-	v.type = SCARB_TRUE;
-	EXPECT_EQ_INT(SCARB_PARSE_ROOT_NOT_SINGULAR, scarb_parse(&v, "null x"));
-	EXPECT_EQ_INT(SCARB_NULL, scarb_get_type(&v));
+	TEST_ERROR(SCARB_PARSE_ROOT_NOT_SINGULAR, "null x");
+
+	/* invalid number */
+	TEST_ERROR(SCARB_PARSE_ROOT_NOT_SINGULAR, "0123"); /* after zero should be '.' or nothing */
+	TEST_ERROR(SCARB_PARSE_ROOT_NOT_SINGULAR, "0x0");
+	TEST_ERROR(SCARB_PARSE_ROOT_NOT_SINGULAR, "0x123");
+}
+
+static void test_parse_number_too_big()
+{
+	TEST_ERROR(SCARB_PARSE_NUMBER_TOO_BIG, "1e309");
+	TEST_ERROR(SCARB_PARSE_NUMBER_TOO_BIG, "-1e309");
 }
 
 static void test_parse()
@@ -79,9 +140,11 @@ static void test_parse()
 	test_parse_null();
 	test_parse_true();
 	test_parse_false();
+	test_parse_number();
 	test_parse_expect_value();
 	test_parse_invalid_value();
 	test_parse_root_not_singular();
+	test_parse_number_too_big();
 }
 
 int main()
